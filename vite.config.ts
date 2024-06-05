f47a2ddf505025e98965feff89d74792a2f241dc
MrNoobCobra <babasahebbhalekar8245@gmail.com>
2024-06-05 15:01:33 +0530

    All looks fine => v1.2 released

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,6 @@ export default defineConfig({
     proxy: {
       "/api": {
         target: "http://localhost:8080",
-        // target: 'http://127.0.0.1:5000',
         secure: false,
       },
     },
